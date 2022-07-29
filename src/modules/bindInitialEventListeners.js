8f8716accf4b289502ad99e2ddefc9c33b0e5fc8
Niels <niels.cpp@gmail.com>
2022-07-29 17:51:29 +0200

    Implement listeners for project add and cancel
    Implemented the event listeners to show project edit form when add project button is clicked.
    Also to hide the project edit form when cancel button is clicked.

--- src/modules/bindInitialEventListeners.js
+++ src/modules/bindInitialEventListeners.js
@@ -3,9 +3,14 @@ import previousDay from "date-fns/previousDay/index";
 function bindInitialEventListeners() {
     // Getting the DOM elements
     const headerProjectsButton = document.querySelector(".header__projects-button");
+    const projectAddButton = document.querySelector(".project__add-button");
+    const projectEditCancelButton = document.querySelector(".project-edit__cancel-button");
+
 
     // Binding listeners
     headerProjectsButton.addEventListener("click", handleHeaderProjectsButtonClick);
+    projectAddButton.addEventListener("click", handleProjectAddButtonClick);
+    projectEditCancelButton.addEventListener("click", handleProjectEditCancelButtonClick);
 }
 
 function handleHeaderProjectsButtonClick(event) {
@@ -45,6 +50,18 @@ function changeHeaderProjectsButtonToOpenMode() {
 
     headerProjectsButton.removeEventListener("click", handleHeaderProjectsButtonCloseClick);
     headerProjectsButton.addEventListener("click", handleHeaderProjectsButtonClick);
+}
+
+function handleProjectAddButtonClick(event) {
+    // Reveal project edit form
+    const projectEditForm = document.querySelector(".project-edit");
+    projectEditForm.classList.remove("project-edit_invisible");
+}
+
+function handleProjectEditCancelButtonClick(event) {
+    // Hide the project edit form again
+    const projectEditForm = document.querySelector(".project-edit");
+    projectEditForm.classList.add("project-edit_invisible");
 }
 
 export default bindInitialEventListeners;
